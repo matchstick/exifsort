5b0a76060ea7bf5692b44cc3f6e0016ca9476674
Michael Rubin <mhr@neverthere.org>
2020-06-07 00:48:49 -0700

    Updating testdir with new requirments
    There is a lot to do with testdir to make it ready for complete testing.
    
    Documenting requirements for upcomig work.

--- testdir/testdir.go
+++ testdir/testdir.go
@@ -9,6 +9,40 @@ import (
 	"testing"
 )
 
+/*
+
+This file began as a quick hack but it is becoming apparrent it is going to
+need work and focus if we are going to put exifsort into real production use
+for the family.
+
+Requirements are accumaltive as the stages of the pipeline refine the needs of
+the input directories.
+
+Scanner Requirements:
+* A directory with two levels
+* Some files (variable) with valid exifdata
+* Some files (variable) with invalid exifdata so they use mod times
+* Some examples of filepath errors
+* Some files to skip
+
+Sort Requirements
+* All the same ones scanner has
+* Create input files to allow for media sets of one and multiple files.
+* Have media sets be spread across all methods
+* Have transfer be all actions.
+* Exercise collision paths
+* Exercise duplicate paths
+
+Merge rerquirments
+* All the ones sorter has
+* Take as input a sorted directory with correct structure
+* A directory with broken structure
+* A directory with a disjoint set of media files to create new leaves
+* A directory with a same set of media files to not add anything to the dst
+* A directory with the same media names and sort times but different contents to create collisions.
+
+*/
+
 const (
 	ExifPath    = "../data/with_exif.jpg"
 	ExifTimeStr = "2020:04:28 14:12:21"
